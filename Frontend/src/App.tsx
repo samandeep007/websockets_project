95ffc27e02cb26a9e79bdab3773a10304435f012
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-02 00:02:41 -0600

    chore: Update WebSocket connection in App component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from 'react'
+
 function App() {
+  const[socket, setSocket] = useState<WebSocket | null>(null) 
   
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080')  //WebSocket comes from the browser
+   
+    ws.onopen = () => {
+      console.log('Connected to server');
+      setSocket(ws)
+    }
+
+    ws.onmessage = (message) => {
+      console.log('Received:', message.data);
+    }
+
+  }, [])
+ 
+
+  if(!socket){
+    return <>
+    <h1>Connecting to the WebSocket server</h1>
+    </>
+  }
+
 
   return (
     <>
